93197afe14c287a4ab1fbfc10bd4929744b0772f
jo0xFF <yousif.dev25th@gmail.com>
2023-02-07 02:51:14 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Clipzy: Share Clips Of competitve games moments shared by people.
+
+
+## The Purpose
+Clipzy is a portfolio project to demonstrate the skills in angular framework.
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCmrgdIyHst2iD0MaZaCxeFjgqSSxtla7I",
+    authDomain: "clipzy-b5b50.firebaseapp.com",
+    projectId: "clipzy-b5b50",
+    storageBucket: "clipzy-b5b50.appspot.com",
+    messagingSenderId: "466552284867",
+    appId: "1:466552284867:web:041dd97c8ea4e31b32e476"
+  }
+};
